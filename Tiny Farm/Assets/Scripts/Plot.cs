23b65d8ad12f530af23380a5327775ac39993630
Roman Wind <119047529+RomanWind@users.noreply.github.com>
2023-11-23 16:42:45 +0100

    Plots Selection Mechanic Setup
    Player now can tap on plot to select it and deselect plot as well with second tap on plot, tap on floor or select another plot by cklicking on it

--- Tiny Farm/Assets/Scripts/Plot.cs
+++ Tiny Farm/Assets/Scripts/Plot.cs
@@ -1,10 +1,71 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Plot : MonoBehaviour, IPointerClickHandler
 {
+	[SerializeField] private GameObject _selectionImage;
+	[SerializeField] private PlotsSelection _plotsSelection;
+
+	private State _state;
+	private float growingTimer;
+
+	public enum State
+	{
+		Empty,
+		Growing,
+		NeedWater,
+		Ready
+	}
+
+	private void Start()
+	{
+		_state = State.Empty;
+	}
+
+	private void Update()
+	{
+		switch(_state)
+		{
+			case State.Empty:
+				break; 
+			case State.Growing:
+				growingTimer += Time.deltaTime;
+			break;
+		}
+	}
+
 	public void OnPointerClick(PointerEventData eventData)
 	{
-		Debug.Log(gameObject.name);
+		SelectPlot();
+	}
+
+	private void SelectPlot()
+	{
+		if (_plotsSelection.GetSelectedPlot() == null)
+		{
+			_selectionImage.SetActive(true);
+			_plotsSelection.SetSelectedPlot(gameObject);
+		}
+		else if (_plotsSelection.GetSelectedPlot() != gameObject)
+		{
+			_plotsSelection.GetSelectedPlot().GetComponent<Plot>().UnselectPlot();
+			_plotsSelection.ResetSelectedPlot();
+
+			_selectionImage.SetActive(true);
+			_plotsSelection.SetSelectedPlot(gameObject);
+		}
+		else
+		{
+			_plotsSelection.GetSelectedPlot().GetComponent<Plot>().UnselectPlot();
+			_plotsSelection.ResetSelectedPlot();
+		}
+	}
+
+	public void UnselectPlot()
+	{
+		_selectionImage.SetActive(false);
+		_state = State.Empty;
 	}
 }
---
+++ Tiny Farm/Assets/Scripts/PlotsSelection.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlotsSelection : MonoBehaviour, IPointerClickHandler
+{
+	[SerializeField] private GameObject _selectedPlot;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log("random click");
+		
+		if (_selectedPlot != null)
+		{
+			Plot plot = _selectedPlot.GetComponent<Plot>();
+			plot.UnselectPlot();
+			_selectedPlot = null;
+		}
+	}
+
+	public GameObject GetSelectedPlot() => _selectedPlot;
+	public void SetSelectedPlot(GameObject plot) => _selectedPlot = plot;
+	public void ResetSelectedPlot() => _selectedPlot = null;
+}
